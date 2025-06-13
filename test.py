780111771f6c7decd8ed3eb6645a6364fe7e849c
xevansz <minithbmatthew@gmail.com>
2025-06-13 13:39:38 +0530

    test: scrap & save data

---
+++ test.py
@@ -0,0 +1,54 @@
+from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
+import time
+import random
+import pandas as pd
+
+def random_delay(min=1, max=3):
+    time.sleep(random.uniform(min, max))
+
+all_novels = []
+
+def scrape_novels(pageNo):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        stealth_sync(page)
+
+        url = f"https://www.novelupdates.com/series-finder/?sf=1&org=495,496,497&sort=sdate&order=desc&pg={pageNo}"
+        print(f"scraping {url}")
+
+        try:
+            page.goto(url)
+            page.wait_for_selector(".search_title")
+            random_delay()
+
+            titles = page.query_selector_all(".search_title")
+            print(f"Found {len(titles)} novels on Page {pageNo}:")
+
+            for title in titles:
+                novel_title = title.inner_text().strip()
+                all_novels.append(novel_title)
+
+        except Exception as e:
+            print(f"error on page {pageNo}: {e}")
+            return False
+
+        browser.close()
+        return True
+
+# driver code
+pageNo = 1
+while pageNo<=100:
+    success = scrape_novels(pageNo)
+    if not success:
+        print(f"Stopping at page {pageNo}")
+        break
+    pageNo+=1
+
+print("Saving the scraped titles into a csv")
+if all(isinstance(title, str) for title in all_novels):
+    df = pd.DataFrame(all_novels, columns=["Title"])
+    df.to_csv("Novel_titles.csv")
+else:
+    print("Error, some titles are not strings!")
