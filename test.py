02b56af8140a903d1bb5c4907201046bf59dbc1e
xevansz <minithbmatthew@gmail.com>
2025-06-13 17:10:44 +0530

    test: case 2

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import time
 import random
 import pandas as pd
 
-def random_delay(min=1, max=3):
+def random_delay(min=1, max=4):
     time.sleep(random.uniform(min, max))
 
 all_novels = []
@@ -16,19 +16,19 @@ def scrape_novels(pageNo):
         stealth_sync(page)
 
         url = f"https://www.novelupdates.com/series-finder/?sf=1&org=495,496,497&sort=sdate&order=desc&pg={pageNo}"
-        print(f"scraping {url}")
+        print(f"scraping page {pageNo}")
 
         try:
             page.goto(url)
             page.wait_for_selector(".search_title")
             random_delay()
 
-            titles = page.query_selector_all(".search_title")
-            print(f"Found {len(titles)} novels on Page {pageNo}:")
+            novels = page.query_selector_all(".search_body_nu")
+            print(f"Found {len(novels)} novels on {pageNo}")
 
-            for title in titles:
-                novel_title = title.inner_text().strip()
-                all_novels.append(novel_title)
+            for novel in novels:
+                titles = novel.query_selector(".search_title").inner_text().strip()
+                print(titles)
 
         except Exception as e:
             print(f"error on page {pageNo}: {e}")
@@ -38,17 +38,22 @@ def scrape_novels(pageNo):
         return True
 
 # driver code
-pageNo = 1
-while pageNo<=100:
-    success = scrape_novels(pageNo)
-    if not success:
-        print(f"Stopping at page {pageNo}")
-        break
-    pageNo+=1
+def main():
+    pageNo = 1
+    while pageNo<=2:
+        success = scrape_novels(pageNo)
+        if not success:
+            print(f"Stopping at page {pageNo}")
+            break
+        pageNo+=1
+    print(f"stoping at {pageNo - 1}")
 
-print("Saving the scraped titles into a csv")
-if all(isinstance(title, str) for title in all_novels):
-    df = pd.DataFrame(all_novels, columns=["Title"])
-    df.to_csv("Novel_titles.csv")
-else:
-    print("Error, some titles are not strings!")
+    print("Saving the scraped titles into a csv")
+    if all(isinstance(title, str) for title in all_novels):
+        df = pd.DataFrame(all_novels, columns=["Title"])
+        df.to_csv("Novel_titles.csv")
+    else:
+        print("Error, some titles are not strings!")
+
+if __name__ == "__main__":
+    main()
